4c9a687fe8bdce988c0bea4152b7577808f04a84
Neha K <nehakhopade3@gmail.com>
2024-08-24 11:10:31 +0530

    java docs added in this class.

--- src/main/java/com/person/mgt/repository/PersonRepository.java
+++ src/main/java/com/person/mgt/repository/PersonRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
+/*
+* this is repository class for person extentions.
+* */
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
 }
